6a97527596177d9f82c7d0c52ea091c59bcaa07d
Shawn Zhang <xiaoen.zhang@mail.utoronto.ca>
2024-01-17 20:15:06 -0500

    3.1

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\shawn\OneDrive\Desktop\rust_projects\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\shawn\OneDrive\Desktop\rust_projects\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\shawn\OneDrive\Desktop\rust_projects\variables\target\debug\variables.exe: C:\Users\shawn\OneDrive\Desktop\rust_projects\variables\src\main.rs
